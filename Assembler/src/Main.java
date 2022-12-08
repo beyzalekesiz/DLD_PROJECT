aac2c6533cb94e1a4fc576139daa96d90d045efe
beyzalekesiz <109971921+beyzalekesiz@users.noreply.github.com>
2022-12-08 16:12:48 +0300

    Update Main.java

--- Assembler/src/Main.java
+++ Assembler/src/Main.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+
 
     private final String[] instructionSet = {"SUB", "SUBI", "ADD", "ADDI", "AND", "ANDI", "OR" ,"ORI",
                                              "XOR", "XORI", "LD", "ST", "JUMP", "PUSH", "POP", "BE", "BNE"};
-
-
+    public static void main(String[] args) {
+     try {
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        String line;
+        while((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
+    } catch (
+    IOException e) {
+        e.printStackTrace();
+    }
+}
 
 }
