73eb033f23feb86724d034bfa80343d3409307a6
beyzalekesiz <109971921+beyzalekesiz@users.noreply.github.com>
2022-12-08 15:19:23 +0300

    Add files via upload

---
+++ Assembler/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    private final String[] instructionSet = {"SUB", "SUBI", "ADD", "ADDI", "AND", "ANDI", "OR" ,"ORI",
+                                             "XOR", "XORI", "LD", "ST", "JUMP", "PUSH", "POP", "BE", "BNE"};
+
+
+
+}
